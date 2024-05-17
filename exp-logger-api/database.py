2f2c768375463fc1d4c6d017da6c694257c14ee1
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2024-05-17 12:15:10 -0300

    Conecction to Heroku Postgres DB

--- exp-logger-api/database.py
+++ exp-logger-api/database.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./exp-logger.db'
+#DATABASE_URL = 'sqlite:///./exp-logger.db'
+DATABASE_URL = 'postgresql://ueo8uc57lmfe61:p4c1dc2568af90009881047aa3d794cc0aa36df7f51a8d8893ab2eac134cef980@ceqbglof0h8enj.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d95bp6njkiql5j'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread" : False})
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
