752485b702f72adc81ad6484433af58515bf0ad4
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2024-05-17 13:24:33 -0300

    Fixing postgresql module name

--- exp-logger-api/database.py
+++ exp-logger-api/database.py
@@ -9,7 +9,7 @@ import os
 IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
 
 if IS_HEROKU_APP:
-    DATABASE_URL = os.environ['DATABASE_URL']
+    DATABASE_URL = os.environ['DATABASE_URL'].replace('postgres','postgresql')
 
 else:
     DATABASE_URL = 'postgresql://ueo8uc57lmfe61:p4c1dc2568af90009881047aa3d794cc0aa36df7f51a8d8893ab2eac134cef980@ceqbglof0h8enj.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d95bp6njkiql5j'
