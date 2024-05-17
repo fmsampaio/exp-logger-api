2158f9cc79492b8efa2d1aca80b99cee046caa9c
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2024-05-17 15:17:55 -0300

    Allowing CORS access

--- exp-logger-api/main.py
+++ exp-logger-api/main.py
@@ -2,9 +2,19 @@ from fastapi import FastAPI
 from .database import Base, engine
 from .routers import log_entry, project
 
+from fastapi.middleware.cors import CORSMiddleware
+
 Base.metadata.create_all(engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 app.include_router(log_entry.router)
 app.include_router(project.router)
